44957e0317c58ba1c02eab0c4b34b3bd953d4f4a
Stewie <nhut170501@gmail.com>
2022-04-08 17:01:59 +0700

    TH CNPM

---
+++ QLSinhVien/Controllers/QLSinhVienController.cs
@@ -0,0 +1,21 @@
+﻿using QLSinhVien.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLSinhVien.Controllers
+{
+    public class QLSinhVienController : Controller
+    {
+        // GET: QLSinhVien
+
+        CNPMEntities DB = new CNPMEntities();
+        public ActionResult Index()
+        {
+            var All_DuLieu = from tt in DB.QLSinhViens select tt;
+            return View(All_DuLieu);
+        }
+    }
+}   
